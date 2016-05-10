96ed5a31d6a94bbf560c5a5dc9adb8bf3ab25496
Lucas Benedicic <benedicic@cscs.ch>
2016-05-10 15:20:21 +0200

    Forcing LC_ALL or application crashes.-

--- connect.sh
+++ connect.sh
@@ -4,7 +4,7 @@ set -e
 
 CONTAINER_ID="$( docker ps -a | grep gazebo:7.1 | awk '{ print $1; }' )"
 CONTAINER_IP="$( docker inspect ${CONTAINER_ID} | grep '\"IPAddress\"' | awk '{ print $2; }' | head -n 1 | tr -d '",' )"
-EXE_CMD='/usr/bin/gazebo'
+EXE_CMD='export LC_ALL=C; /usr/bin/gazebo'
 
 echo ">>> Gazebo running in container at ${CONTAINER_IP} <<<"
 ssh -YC -i ./ssh/id_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null docker@${CONTAINER_IP} "${EXE_CMD}"
