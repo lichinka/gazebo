55fbb1e1bea40b3c7227c40a6ae281d4d48c6d93
Kean Mariotti <mkean@greina21.cm.cluster>
2016-07-29 15:52:39 +0200

    scl7.2: cleaned up gazebo7_install.sh

--- scl7.2/gazebo7_install.sh
+++ scl7.2/gazebo7_install.sh
@@ -1,32 +1,13 @@
 #!/bin/bash
 
-SCRIPT_PATH=$(pwd)
-PREFIX=$HOME/local
-SYSCONFDIR=$PREFIX/etc
+SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $SCRIPT_PATH/load-environment
 mkdir -p $PREFIX
-TEMP=$HOME/tmp
 mkdir -p $TEMP
-
 mkdir -p $PREFIX/include
 mkdir -p $PREFIX/lib
 
-NUM_OF_PROCESSORS=$(grep -c ^processor /proc/cpuinfo)
-
-export ACLOCAL="aclocal -I ${PREFIX}/share/aclocal"
-export PATH=$PREFIX/bin:$PATH
-export PKG_CONFIG_PATH=$PREFIX/lib64/pkgconfig:$PREFIX/lib/pkgconfig:$PREFIX/share/pkgconfig:$PKG_CONFIG_PATH
-export LIBRARY_PATH=$PREFIX/lib:$PREFIX/lib64
-export LD_LIBRARY_PATH=$LIBRARY_PATH
-export C_INCLUDE_PATH=$PREFIX/include:$C_INCLUDE_PATH
-export CPLUS_INCLUDE_PATH=$C_INCLUDE_PATH
-
-#base dependiencies
-module load CMake/3.4.1-GCC-4.9.2
-module load Autotools/20150215-GCC-4.9.3-2.25
-module load intel-tbb-oss/intel64/44_20160526oss
-#probably useless module load cuda70/toolkit/7.0.28 
-
-#install packages with standard configuration
+#automatically install packages with configure.sh
 function install {
    url=$1
    archive=$(basename "$1")
@@ -41,14 +22,12 @@ function install {
    make install 2>&1 | tee output-make-install
 }
 
-#ignition math
-cd $TEMP
-wget https://bitbucket.org/ignitionrobotics/ign-math/get/ignition-math2_2.5.0.tar.bz2
-tar xvf ignition-math2_2.5.0.tar.bz2
-cd ignitionrobotics-ign-math-6c0e329b4808 && mkdir -p build && cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX
-make -j$NUM_OF_PROCESSORS
-make install
+#libcurl
+export PKG_CONFIG_PATH=/cm/local/apps/curl/lib/pkgconfig:$PKG_CONFIG_PATH
+export C_INCLUDE_PATH=/cm/local/apps/curl/include/:$C_INCLUDE_PATH
+export CPLUS_INCLUDE_PATH=/cm/local/apps/curl/include/:$CPLUS_INCLUDE_PATH
+export LIBRARY_PATH=/cm/local/apps/curl/lib:$LIBRARY_PATH
+export LD_LIBRARY_PATH=/cm/local/apps/curl/lib:$LD_LIBRARY_PATH
 
 #tinyxml
 cd $TEMP
@@ -74,6 +53,15 @@ cmake -DCMAKE_INSTALL_PREFIX=$PREFIX ..
 make -j$NUM_OF_PROCESSORS 
 make install
 
+#ignition math
+cd $TEMP
+wget https://bitbucket.org/ignitionrobotics/ign-math/get/ignition-math2_2.5.0.tar.bz2
+tar xvf ignition-math2_2.5.0.tar.bz2
+cd ignitionrobotics-ign-math-6c0e329b4808 && mkdir -p build && cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX
+make -j$NUM_OF_PROCESSORS
+make install
+
 #SD format
 cd $TEMP
 wget https://bitbucket.org/osrf/sdformat/get/sdformat4_4.1.1.tar.bz2
@@ -83,30 +71,14 @@ cmake -DCMAKE_INSTALL_PREFIX=$PREFIX ..
 make -j$NUM_OF_PROCESSORS
 make install
 
-#protobuf
-cd $TEMP
-wget https://github.com/google/protobuf/archive/v3.0.0-beta-4.tar.gz
-tar xvf v3.0.0-beta-4.tar.gz
-cd protobuf-3.0.0-beta-4
-./autogen.sh
-./configure --prefix=$PREFIX
-make -j$NUM_OF_PROCESSORS 
-make install
-
 #freeimage
 cd $TEMP
 wget https://sourceforge.net/projects/freeimage/files/Source%20Distribution/3.17.0/FreeImage3170.zip
 unzip FreeImage3170.zip 
 cd FreeImage
+make -j$NUM_OF_PROCESSORS
 cp Dist/lib* $PREFIX/lib 
 cp Dist/FreeImage.h $PREFIX/include
-
-#libcurl
-export PKG_CONFIG_PATH=/cm/local/apps/curl/lib/pkgconfig:$PKG_CONFIG_PATH
-export C_INCLUDE_PATH=/cm/local/apps/curl/include/:$C_INCLUDE_PATH
-export CPLUS_INCLUDE_PATH=/cm/local/apps/curl/include/:$CPLUS_INCLUDE_PATH
-export LIBRARY_PATH=/cm/local/apps/curl/lib:$LIBRARY_PATH
-export LD_LIBRARY_PATH=/cm/local/apps/curl/lib:$LD_LIBRARY_PATH
 
 ##libtar
 cd $TEMP
@@ -121,6 +93,29 @@ autoreconf --force --install
 make -j$NUM_OF_PROCESSORS
 make install
 
+install http://downloads.sourceforge.net/libpng/libpng-1.6.23.tar.xz
+
+#freetype (without harfbuz)
+cd $TEMP
+wget http://download.savannah.gnu.org/releases/freetype/freetype-2.6.tar.gz
+tar xvf freetype-2.6.tar.gz
+cd freetype-2.6
+./configure --prefix=$PREFIX --without-harfbuzz
+make -j$NUM_OF_PROCESSORS
+make install
+
+#install https://www.freedesktop.org/software/harfbuzz/release/harfbuzz-1.3.0.tar.bz2
+##freetype (again, this time with harfbuzz)
+#install http://download.savannah.gnu.org/releases/freetype/freetype-2.6.tar.gz
+
+#fontconfig
+cd $TEMP
+wget https://www.freedesktop.org/software/fontconfig/release/fontconfig-2.12.0.tar.gz
+tar xvf fontconfig-2.12.0.tar.gz
+cd fontconfig-2.12.0
+./configure --prefix=$PREFIX --enable-libxml2
+make -j$NUM_OF_PROCESSORS
+make install
 
 #xorg
 install https://www.x.org/releases/individual/util/util-macros-1.19.0.tar.gz
@@ -166,83 +161,23 @@ install https://www.x.org/releases/individual/lib/pixman-0.34.0.tar.gz
 install https://www.x.org/releases/individual/lib/libXau-1.0.8.tar.gz
 
 #install https://xcb.freedesktop.org/dist/xcb-proto-1.12.tar.gz
-
 #install https://xcb.freedesktop.org/dist/libpthread-stubs-0.3.tar.gz
-
 #install https://xcb.freedesktop.org/dist/libxcb-1.12.tar.gz
-
-#install https://sourceforge.net/projects/libpng/files/libpng16/1.6.23/libpng-1.6.23.tar.gz
-
-#probably useless
-#install https://www.cairographics.org/releases/cairo-1.14.6.tar.xz
-
 #install ftp://sourceware.org/pub/libffi/libffi-3.2.tar.gz
-
 #install http://ftp.gnome.org/pub/GNOME/sources/glib/2.49/glib-2.49.4.tar.xz
-
-#probably useless
-##freetype (without harfbuz)
-#cd $TEMP
-#wget http://download.savannah.gnu.org/releases/freetype/freetype-2.6.tar.gz
-#tar xvf freetype-2.6.tar.gz
-#cd freetype-2.6
-#./configure --prefix=$PREFIX --without-harfbuzz
-#make -j$NUM_OF_PROCESSORS
-#make install
-
-#probably useless
-#install https://www.freedesktop.org/software/harfbuzz/release/harfbuzz-1.3.0.tar.bz2
-
-#probably useless
-##freetype (again, this time with harfbuzz)
-#install http://download.savannah.gnu.org/releases/freetype/freetype-2.6.tar.gz
-
-#probably useless
-##fontconfig
-#cd $TEMP
-#wget https://www.freedesktop.org/software/fontconfig/release/fontconfig-2.12.0.tar.gz
-#tar xvf fontconfig-2.12.0.tar.gz
-#cd fontconfig-2.12.0
-#./configure --prefix=$PREFIX --enable-libxml2
-#make -j$NUM_OF_PROCESSORS
-#make install
-
-#probably useless
-##cairo (again, this time with freetype)
 #install https://www.cairographics.org/releases/cairo-1.14.6.tar.xz
-
-#probably useless
 #install http://ftp.gnome.org/pub/GNOME/sources/pango/1.40/pango-1.40.1.tar.xz
 
-install http://www.nasm.us/pub/nasm/releasebuilds/2.12/nasm-2.12.tar.gz
-install https://sourceforge.net/projects/libjpeg-turbo/files/1.5.0/libjpeg-turbo-1.5.0.tar.gz
-install ftp://ftp.remotesensing.org/pub/libtiff/tiff-4.0.6.tar.gz
+#install http://www.nasm.us/pub/nasm/releasebuilds/2.12/nasm-2.12.tar.gz
+#install https://sourceforge.net/projects/libjpeg-turbo/files/1.5.0/libjpeg-turbo-1.5.0.tar.gz
+#install ftp://ftp.remotesensing.org/pub/libtiff/tiff-4.0.6.tar.gz
 
-#probably useless
-#install https://download.gnome.org/sources/gdk-pixbuf/2.35/gdk-pixbuf-2.35.2.tar.xz
-
-#probably useless
-#install https://download.gnome.org/sources/atk/2.20/atk-2.20.0.tar.xz
-
-#probably useless
-#install http://launchpad.net/intltool/trunk/0.50.2/+download/intltool-0.50.2.tar.gz
-
-#probably useless
-#install https://sourceforge.net/projects/expat/files/expat/2.2.0/expat-2.2.0.tar.bz2
-
-install http://dbus.freedesktop.org/releases/dbus/dbus-1.10.6.tar.gz
-
-#probably useless
-#install http://ftp.gnome.org/pub/gnome/sources/at-spi2-core/2.18/at-spi2-core-2.18.3.tar.xz
-#install http://ftp.gnome.org/pub/gnome/sources/at-spi2-atk/2.18/at-spi2-atk-2.18.1.tar.xz
-#install http://ftp.gnome.org/pub/gnome/sources/gtk+/3.20/gtk+-3.20.6.tar.xz
-
-#TODO: this is useless! get rid of it and all the dependencies
-#install http://www.joachim-breitner.de/archive/screen-message/screen-message-0.24.tar.gz
+#install http://dbus.freedesktop.org/releases/dbus/dbus-1.10.6.tar.gz
 
 #ogre
+mkdir -p $PREFIX/include/GL && ln -s /cm/shared/apps/easybuild.org-2.6/software/CUDA/7.5.18/extras/CUPTI/include/GL/glu.h $PREFIX/include/GL/glu.h #TODO: get rid of this hack
 cd $TEMP
-wget https://bitbucket.org/sinbad/ogre/get/v1-9-0.tar.bz2 -o ogre-v1-9-0.tar.bz2
+wget https://bitbucket.org/sinbad/ogre/get/v1-9-0.tar.bz2 -O ogre-v1-9-0.tar.bz2
 tar xvf ogre-v1-9-0.tar.bz2 
 cd sinbad-ogre-dd30349ea667
 [[ -d build ]] && rm -rf build
@@ -252,13 +187,14 @@ cmake .. \
     -DOGRE_INSTALL_SAMPLES=FALSE \
     -DOGRE_INSTALL_DOCS=FALSE \
     -DOGRE_INSTALL_SAMPLES_SOURCE=FALSE \
-    -DCMAKE_BUILD_TYPE=Release
-make -j$NUM_OF_PROCESSORS
+    -DCMAKE_BUILD_TYPE=Release \
+    2>&1 | tee output-cmake
+make -j$NUM_OF_PROCESSORS 2>&1 | tee output-make
 make install
 
-install https://gstreamer.freedesktop.org/src/gstreamer/gstreamer-0.10.36.tar.gz
-install https://gstreamer.freedesktop.org/src/gst-plugins-base/gst-plugins-base-0.10.36.tar.gz
-install ftp://ftp.alsa-project.org/pub/lib/alsa-lib-1.0.29.tar.bz2
+#install https://gstreamer.freedesktop.org/src/gstreamer/gstreamer-0.10.36.tar.gz
+#install https://gstreamer.freedesktop.org/src/gst-plugins-base/gst-plugins-base-0.10.36.tar.gz
+#install ftp://ftp.alsa-project.org/pub/lib/alsa-lib-1.0.29.tar.bz2
 
 #qt
 cd $TEMP
@@ -269,6 +205,20 @@ yes | ./configure --prefix=$PREFIX -opensource 2>&1 | tee output-configure
 make -j$NUM_OF_PROCESSORS 2>&1 | tee output-make
 make install 2>&1 | tee output-makeinstall
 
+#protobuf
+cd $TEMP
+wget https://github.com/google/protobuf/archive/v3.0.0-beta-4.tar.gz
+tar xvf v3.0.0-beta-4.tar.gz
+cd protobuf-3.0.0-beta-4
+./autogen.sh
+./configure --prefix=$PREFIX
+make -j$NUM_OF_PROCESSORS 
+make install
+
+install ftp://sourceware.org/pub/libffi/libffi-3.2.1.tar.gz
+install http://ftp.gnome.org/pub/gnome/sources/glib/2.48/glib-2.48.1.tar.xz
+install https://sourceforge.net/projects/gts/files/gts/0.7.6/gts-0.7.6.tar.gz
+
 #gazebo
 cd $TEMP
 wget https://bitbucket.org/osrf/gazebo/get/gazebo7_7.3.1.tar.bz2
@@ -278,6 +228,6 @@ patch < $SCRIPT_PATH/patches/gazebo/CMakeLists.txt.patch
 [[ -d build ]] && rm -rf build
 mkdir -p build && cd build
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -DENABLE_TESTS_COMPILATION:BOOL=False .. 2>&1 | tee output-cmake
-make -j$NUM_OF_PROCESSORS 
+make -j$NUM_OF_PROCESSORS 2>&1 | tee output-make
 make install
 
