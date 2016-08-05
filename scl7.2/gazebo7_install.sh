80448e5a467328a704e4e1000485811f7811fe54
Kean Mariotti <mkean@greina20.cm.cluster>
2016-08-05 11:34:10 +0200

    add hack for ogre's cmake (can't find freetype without it)

--- scl7.2/gazebo7_install.sh
+++ scl7.2/gazebo7_install.sh
@@ -187,6 +187,7 @@ install https://www.x.org/releases/individual/lib/libXau-1.0.8.tar.gz
 
 #ogre
 mkdir -p $PREFIX/include/GL && ln -s /cm/shared/apps/easybuild.org-2.6/software/CUDA/7.5.18/extras/CUPTI/include/GL/glu.h $PREFIX/include/GL/glu.h #TODO: get rid of this hack
+mkdir -p $PREFIX/include/freetype2/freetype && cp -r $PREFIX/include/freetype2/* $PREFIX/include/freetype2/freetype #hack (ogre's cmake searches in freetype2/freetype dir)
 cd $TEMP
 wget wget https://bitbucket.org/sinbad/ogre/get/v1-8-1.tar.gz
 tar xvf v1-8-1.tar.gz
