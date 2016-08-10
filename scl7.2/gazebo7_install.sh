49ef04776574fde7795580d62a388987318d2d08
Lucas Benedicic <benedicic@cscs.ch>
2016-08-10 09:37:54 +0200

    Various fixes to speed the process up and avoid failures when re-running.-

--- scl7.2/gazebo7_install.sh
+++ scl7.2/gazebo7_install.sh
@@ -122,9 +122,12 @@ make install
 install http://downloads.sourceforge.net/libpng/libpng-1.6.23.tar.xz
 
 #freetype (without harfbuz)
+rm -rf "$PREFIX/include/freetype2/freetype"
 cd $TEMP
-wget http://download.savannah.gnu.org/releases/freetype/freetype-2.6.tar.gz
-tar xvf freetype-2.6.tar.gz
+if [ ! -d "freetype-2.6" ]; then
+   wget http://download.savannah.gnu.org/releases/freetype/freetype-2.6.tar.gz
+   tar xvf freetype-2.6.tar.gz
+fi
 cd freetype-2.6
 ./configure --prefix=$PREFIX --without-harfbuzz
 make -j$NUM_OF_PROCESSORS
@@ -132,14 +135,18 @@ make install
 
 #fontconfig
 cd $TEMP
-wget https://www.freedesktop.org/software/fontconfig/release/fontconfig-2.12.0.tar.gz
-tar xvf fontconfig-2.12.0.tar.gz
+if [ ! -d "fontconfig-2.12.0" ]; then
+   wget https://www.freedesktop.org/software/fontconfig/release/fontconfig-2.12.0.tar.gz
+   tar xvf fontconfig-2.12.0.tar.gz
+fi
 cd fontconfig-2.12.0
 ./configure --prefix=$PREFIX --enable-libxml2
 make -j$NUM_OF_PROCESSORS
 make install
 
 #xorg
+rm "$PREFIX/include/GL/glu.h"
+
 install https://www.x.org/releases/individual/util/util-macros-1.19.0.tar.gz
 
 install https://www.x.org/releases/individual/proto/xproto-7.0.29.tar.gz
@@ -193,16 +200,23 @@ install https://www.x.org/archive/individual/lib/libxshmfence-1.2.tar.gz
 install https://www.x.org/releases/individual/lib/pixman-0.34.0.tar.gz
 install https://www.x.org/releases/individual/lib/libXau-1.0.8.tar.gz
 
-#ogre
-mkdir -p $PREFIX/include/GL && ln -s /cm/shared/apps/easybuild.org-2.6/software/CUDA/7.5.18/extras/CUPTI/include/GL/glu.h $PREFIX/include/GL/glu.h #hack (no other glu.h available)
-mkdir -p $PREFIX/include/freetype2/freetype && cp -r $PREFIX/include/freetype2/* $PREFIX/include/freetype2/freetype #hack (ogre's cmake searches in freetype2/freetype dir)
+##ogre3d
+mkdir -p $PREFIX/include/GL
+if [ ! -f "$PREFIX/include/GL/glu.h" ]; then
+   ln -s /cm/shared/apps/easybuild.org-2.6/software/CUDA/7.5.18/extras/CUPTI/include/GL/glu.h $PREFIX/include/GL/glu.h #hack (no other glu.h available)
+fi
+#ogre's cmake searches in freetype2/freetype dir
+cd $PREFIX/include/freetype2 && ln -s ../freetype2 freetype
 cd $TEMP
-wget wget https://bitbucket.org/sinbad/ogre/get/v1-8-1.tar.gz
-tar xvf v1-8-1.tar.gz
+if [ ! -d "sinbad-ogre-525a7f3bcd4e" ]; then
+   wget https://bitbucket.org/sinbad/ogre/get/v1-8-1.tar.gz
+   tar xvzf v1-8-1.tar.gz
+fi
 cd sinbad-ogre-525a7f3bcd4e
 [[ -d build ]] && rm -rf build
 mkdir build && cd build
 cmake .. \
+    -DCMAKE_CXX_FLAGS="-Wno-narrowing" \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DOGRE_INSTALL_SAMPLES=FALSE \
     -DOGRE_INSTALL_DOCS=FALSE \
@@ -212,36 +226,50 @@ cmake .. \
 make -j$NUM_OF_PROCESSORS 2>&1 | tee output-make
 make install
 
+##expat
 install https://sourceforge.net/projects/expat/files/expat/2.2.0/expat-2.2.0.tar.bz2
+
+##dbus
 install http://dbus.freedesktop.org/releases/dbus/dbus-1.10.6.tar.gz
 
-#qt
+##qt
 cd $TEMP
-wget http://download.qt-project.org/official_releases/qt/4.8/4.8.7/qt-everywhere-opensource-src-4.8.7.tar.gz
-tar xvf qt-everywhere-opensource-src-4.8.7.tar.gz
+if [ ! -d "qt-everywhere-opensource-src-4.8.7" ]; then
+   wget http://download.qt-project.org/official_releases/qt/4.8/4.8.7/qt-everywhere-opensource-src-4.8.7.tar.gz
+   tar xvf qt-everywhere-opensource-src-4.8.7.tar.gz
+fi
 cd qt-everywhere-opensource-src-4.8.7
 yes | ./configure --prefix=$PREFIX -opensource 2>&1 | tee output-configure
 make -j$NUM_OF_PROCESSORS 2>&1 | tee output-make
 make install 2>&1 | tee output-makeinstall
 
-#protobuf
+##protobuf
 cd $TEMP
-wget https://github.com/google/protobuf/archive/v3.0.0-beta-4.tar.gz
-tar xvf v3.0.0-beta-4.tar.gz
+if [ ! -d "protobuf-3.0.0-beta-4" ]; then
+   wget https://github.com/google/protobuf/archive/v3.0.0-beta-4.tar.gz
+   tar xvf v3.0.0-beta-4.tar.gz
+fi
 cd protobuf-3.0.0-beta-4
 ./autogen.sh 2>&1 | tee output-autogen
 ./configure --prefix=$PREFIX 2>&1 | tee output-configure
 make -j$NUM_OF_PROCESSORS  2>&1 | tee output-make
-make install
+make install 2>&1 | tee output-makeinstall
 
+##libffi
 install ftp://sourceware.org/pub/libffi/libffi-3.2.1.tar.gz
+
+##glib
 install http://ftp.gnome.org/pub/gnome/sources/glib/2.48/glib-2.48.1.tar.xz
+
+##gts
 install https://sourceforge.net/projects/gts/files/gts/0.7.6/gts-0.7.6.tar.gz
 
-#gazebo
+##gazebo
 cd $TEMP
-wget https://bitbucket.org/osrf/gazebo/get/gazebo7_7.3.1.tar.bz2
-tar xvf gazebo7_7.3.1.tar.bz2
+if [ ! -d "osrf-gazebo-9bc8cb494795" ]; then
+   wget https://bitbucket.org/osrf/gazebo/get/gazebo7_7.3.1.tar.bz2
+   tar xvf gazebo7_7.3.1.tar.bz2
+fi
 cd osrf-gazebo-9bc8cb494795
 patch < $SCRIPT_PATH/patches/gazebo/CMakeLists.txt.patch
 [[ -d build ]] && rm -rf build
